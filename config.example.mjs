bc5c191920ff2db12c35d5096a3b8f36d39d10df
Faurin <111374977+FaurinFox@users.noreply.github.com>
2024-03-27 00:38:24 +0200

    Provide example config file

---
+++ config.example.mjs
@@ -0,0 +1,10 @@
+const Config = {
+    port: 6884, // Change as desired
+    discord: {
+        client_id: "YOUR_CLIENT_ID",
+        client_secret: "YOUR_SECRET_TOKEN",
+        oauth_url: "YOUR_OAUTH_URL",
+    }
+}
+
+export default Config;
