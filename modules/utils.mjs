0bb3d3142dab725573d6c5c5aaac4c6629a4c01b
Faurin <111374977+FaurinFox@users.noreply.github.com>
2024-03-27 15:19:26 +0200

    Add LOG function to utils

--- modules/utils.mjs
+++ modules/utils.mjs
@@ -29,8 +29,37 @@ const Utilities = {
                 break;
         }
     },
+    LOG: function (logMsg, type) {
+        if (typeof logMsg !== "string") return;
+        if (typeof type === "string") {
+            type = type.toLowerCase();
+        }
+        switch (type) {
+            case undefined:
+                console.info(logMsg);
+                break;
+            case 'log':
+                console.log(logMsg);
+                break;
+            case 'info':
+                console.info(logMsg);
+                break;
+            case 'warn':
+                console.warn(logMsg);
+                break;
+            case 'error':
+                console.error(logMsg);
+                break;
+            default:
+                console.info(logMsg);
+                break;
+        }
+    },
     get debug() {
         return this.DEBUG;
+    },
+    get log() {
+        return this.LOG;
     }
 }
 
