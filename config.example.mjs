e272da215960dcbc0ba5cb1f2143343c970e4ee1
Faurin <111374977+FaurinFox@users.noreply.github.com>
2024-03-27 16:13:50 +0200

    Add a comment to example config, remove trailing comma from last entry

--- config.example.mjs
+++ config.example.mjs
@@ -2,9 +2,10 @@ const Config = {
     port: 6884, // Change as desired
     debug: false, // To log debug things or to not log debug things
     discord: {
+        // These are checked, must not match placeholders for this to run
         client_id: "YOUR_CLIENT_ID",
         client_secret: "YOUR_SECRET_TOKEN",
-        oauth_url: "YOUR_OAUTH_URL",
+        oauth_url: "YOUR_OAUTH_URL"
     }
 }
 
