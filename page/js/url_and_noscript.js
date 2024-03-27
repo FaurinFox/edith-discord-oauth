45a9de06040679b80279d79ac778c2f421305cf2
Faurin <111374977+FaurinFox@users.noreply.github.com>
2024-03-27 15:40:09 +0200

    Page JS: check element exists before changing, because on some pages it may not(discord-auth vs index.html, btn 'a' link)

--- page/js/url_and_noscript.js
+++ page/js/url_and_noscript.js
@@ -1,6 +1,14 @@
+function checkElementById(el) {
+    const element = document.getElementById(el);
+    if (element !== null) return true;
+    else return false;
+}
+
 function container_styles() {
-    document.getElementById("container").style.display = 'flex';
-    document.getElementById("noscript-container").style.display = 'none';
+    if (checkElementById("container"))
+        document.getElementById("container").style.display = 'flex';
+    if (checkElementById("noscript-container"))
+        document.getElementById("noscript-container").style.display = 'none';
 }
 
 fetch('/cfg/oauth')
@@ -8,8 +16,10 @@ fetch('/cfg/oauth')
     return response.json();
 })
 .then((data) => {
-    document.getElementById("login-btn").href = data.result.oauth.url;
-    container_styles();
+    if (checkElementById("login-btn")) {
+        document.getElementById("login-btn").href = data.result.oauth.url;
+        container_styles();
+    }
 })
 .catch((err) => {
     console.error(`Error fetching data: ${err}`);
